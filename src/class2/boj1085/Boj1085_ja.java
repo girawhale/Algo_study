d89e7732c2d82f31c58b2c7d64f563609bc9c4c2
JinA <JinA@host.docker.internal>
2020-09-01 10:52:31 +0900

    [JA] 20.09.01 - BOJ 1085

---
+++ src/class2/boj1085/Boj1085_ja.java
@@ -0,0 +1,24 @@
+package class2.boj1085;
+
+import java.util.Scanner;
+
+public class Boj1085_ja{
+	/**
+	 * 한수(x,y) 
+	 * 작사각형 왼쪽 아래 꼭짓점 (0,0) 
+	 * 오른쪽 위 꼭짓점 (w,h) 
+	 * result : 직사각형의 경계선까지 가는 거리의 최솟값
+	 */
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int[] hansu = { in.nextInt(), in.nextInt() };
+		int[] rectangle = { in.nextInt(), in.nextInt() };
+
+		int result = Math.min(Math.min(hansu[0], rectangle[0] - hansu[0]), Math.min(hansu[1], rectangle[1] - hansu[1]));
+
+		System.out.println(result);
+
+	}
+
+}
--- src/class2/boj1085/README.md
+++ src/class2/boj1085/README.md
@@ -18,8 +18,16 @@ x, w-x, y, h-y 의 최소값
 
 - HEE와 동일
 
+---
 
 ### :octocat: Taekyung2
 ![image](https://user-images.githubusercontent.com/37056992/91459968-3e9d0a00-e8c2-11ea-91a4-34ad033b0a6f.png)
 
 - girawhale과 동일 
+
+---
+
+### 😊 JinA
+![image](https://user-images.githubusercontent.com/52408122/91785610-beb7cc80-ec40-11ea-93c6-809b306c16a8.png)
+
+- Taekyung2과 동일
